3c8cc53d060b6eb6846083037f365b37b58ca597
Rémy <remy.villulles@gmail.com>
2018-06-09 02:59:47 +0200

    Added react native camera on project Created tab bar with 3 tabs on main page

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,6 +1,13 @@
 import React from "react"
-import {Text, View} from "react-native";
+import {Text, View, Dimensions, StyleSheet, Image} from "react-native";
+import TabNavigator from "react-native-tab-navigator"
+import ProfilePage from "../profile/ProfilePage";
+import UploadPage from "../upload/UploadPage";
+import SocialPage from "../social/SocialPage";
 
+const screenDim = Dimensions.get("window");
+const imageSize = screenDim.height / 30
+const tabBarHeight = screenDim.height / 12
 
 
 export default class HomePage extends React.Component{
@@ -9,12 +16,75 @@ export default class HomePage extends React.Component{
         header: null,
     };
 
+    constructor(props){
+        super(props)
+
+        this.state = {selected : "profile"}
+    }
+
     render(){
         return(
-            <View>
-                <Text>HomePage</Text>
+            <View style={{flex : 1}}>
+                <TabNavigator tabBarStyle={styles.tabbarstyle}>
+
+                    <TabNavigator.Item
+                        selected={this.state.selected === 'profile'}
+                        title="Profil"
+                        selectedTitleStyle={styles.selectedTitleStyle}
+                        titleStyle={styles.titleStyle}
+                        renderIcon={() => <Image style={{width : imageSize, height: imageSize}} source={require('../../../resources/unselectedprofile.png')} />}
+                        renderSelectedIcon={() => <Image style={{width : imageSize, height: imageSize}} source={require("../../../resources/selectedprofile.png")} />}
+                        onPress={() => this.setState({selected : "profile"})}>
+                        <ProfilePage navigation={this.props.navigation}/>
+                    </TabNavigator.Item>
+
+                    <TabNavigator.Item
+                        selected={this.state.selected === 'upload'}
+                        title="Upload"
+                        selectedTitleStyle={styles.selectedTitleStyle}
+                        titleStyle={styles.titleStyle}
+                        renderIcon={() => <Image style={{width : imageSize, height: imageSize}} source={require('../../../resources/unselectedupload.png')} />}
+                        renderSelectedIcon={() => <Image style={{width : imageSize, height: imageSize}} source={require("../../../resources/selectedupload.png")} />}
+                        onPress={() => this.setState({selected : "upload"})}>
+                        <UploadPage navigation={this.props.navigation}/>
+                    </TabNavigator.Item>
+
+                    <TabNavigator.Item
+                        selected={this.state.selected === 'social'}
+                        title="Social"
+                        selectedTitleStyle={styles.selectedTitleStyle}
+                        titleStyle={styles.titleStyle}
+                        renderIcon={() => <Image style={{width : imageSize, height: imageSize}} source={require('../../../resources/unselectedchat.png')} />}
+                        renderSelectedIcon={() => <Image style={{width : imageSize, height: imageSize}} source={require("../../../resources/selectedchat.png")} />}
+                        onPress={() => this.setState({selected : "social"})}>
+                        <SocialPage navigation={this.props.navigation}/>
+                    </TabNavigator.Item>
+
+                </TabNavigator>
             </View>
         )
     }
 
 }
+
+const styles = StyleSheet.create({
+    tabbarstyle: {
+        height: tabBarHeight,
+        backgroundColor: "white",
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 2},
+        shadowOpacity: 0.8,
+        shadowRadius: 0,
+        elevation: 2,
+    },
+    selectedTitleStyle : {
+        color: "black",
+        fontSize : screenDim.height / 65,
+        marginBottom: screenDim.height / 80
+    },
+    titleStyle : {
+        color: "#969faa",
+        fontSize : screenDim.height / 65,
+        marginBottom: screenDim.height / 80
+    }
+})
---
+++ src/pages/upload/UploadPage.js
@@ -0,0 +1,15 @@
+import React from "react"
+import {Text, View} from "react-native";
+
+
+export default class UploadPage extends React.Component{
+
+    render(){
+        return(
+            <View>
+                <Text>Upload page</Text>
+            </View>
+        )
+    }
+
+}
